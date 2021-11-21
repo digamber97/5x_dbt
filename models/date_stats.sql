5db2e39dd8ef01660fde01c9c1229a8d6d85b9cf
digamber.kalivemula@gmail.com <digamber.kalivemula@gmail.com>
2021-11-21 13:48:17 +0000

    updates

--- models/date_stats.sql
+++ models/date_stats.sql
@@ -7,5 +7,5 @@ with date_stats as (
 "INTERVIEW_DB"."PLAYGROUND_DIGAMBER_KALIVEMULA"."COVID19_DATA" as data on pro.PROVINCE = data.PROVINCE GROUP BY data.PROVINCE
 )
 
-select cases.*, data.DATE from max_cases as cases INNER JOIN "INTERVIEW_DB"."PLAYGROUND_DIGAMBER_KALIVEMULA"."COVID19_DATA" as data 
+select cases.*, data.DATE from date_stats as cases INNER JOIN "INTERVIEW_DB"."PLAYGROUND_DIGAMBER_KALIVEMULA"."COVID19_DATA" as data 
 on cases.higest_cases=data.NEW_CASES and data.PROVINCE= cases.PROVINCE ORDER BY data.DATE
