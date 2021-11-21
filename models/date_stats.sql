58a67dbecf1b592cee10725249740ff55c28dbef
KALIVEMULA <digamber.kalivemula@gmail.com>
2021-11-21 13:01:04 +0000

    added_stast

---
+++ models/date_stats.sql
@@ -0,0 +1,11 @@
+{{ config(materialized = 'view') }}
+
+-- WE ARE TRYING to ANALYZE, IN HWICH PERIOD MOST OF PROVINCE GOT HIGHEST NEW CASES 
+
+with max_cases as (
+    select data.PROVINCE,  max(NEW_CASES) as higest_cases from "INTERVIEW_DB"."PLAYGROUND_DIGAMBER_KALIVEMULA"."PROVINCES" as pro INNER JOIN 
+"INTERVIEW_DB"."PLAYGROUND_DIGAMBER_KALIVEMULA"."COVID19_DATA" as data on pro.PROVINCE = data.PROVINCE GROUP BY data.PROVINCE
+)
+
+select cases.*, data.DATE from max_cases as cases INNER JOIN "INTERVIEW_DB"."PLAYGROUND_DIGAMBER_KALIVEMULA"."COVID19_DATA" as data 
+on cases.higest_cases=data.NEW_CASES and data.PROVINCE= cases.PROVINCE ORDER BY data.DATE
